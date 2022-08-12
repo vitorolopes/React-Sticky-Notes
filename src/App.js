5475bf5823165adac13e67ec58671b96a48f1083
vitor lopes <lopesvitor959@gmail.com>
2022-08-12 11:35:36 +0100

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import './App.scss';
+
+const initialState = {lastNoteCreated: null, notes: [] }
+
+const reducer = (prevState, action) => {
+  switch(action.type){
+    // TODO: ADD_NOTE
+  }
+}
 
 function App() {
+
+  const [noteInput, setNoteInput] = useState(""); 
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const addNote = (e) => { 
+    e.preventDefault()
+    if(!noteInput) return // We don't want to add a blank note 
+    const newNote = {
+      text: noteInput,
+      id: uuidv4(), // going to need an id to delete notes
+      // TODO: number of degrees(rotation) to randomly apply to this note
+    }
+    console.log(newNote);
+    // TODO: dispatch action (add this note to state.notes)
+    setNoteInput("")
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Sticky Notes</h1>
+      <form className="note-form" onSubmit={addNote}>
+        <textarea placeholder='Create a new note ...'
+                  value={noteInput}
+                  onChange={(e)=> setNoteInput(e.target.value)}
+        ></textarea>
+        <button>
+          Add
+        </button>
+      </form>
     </div>
   );
 }
