17c1582a0b8ba4c11a6dadea7d1b25efb8d79483
vitor lopes <lopesvitor959@gmail.com>
2022-08-12 14:49:10 +0100

    Dispatch the DELETE_NOTE action

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ const reducer = (prevState, action) => {
       }
       return newState
     }
+    case "DELETE_NOTE":{
+      const newState = {
+        ...prevState,
+        notes: prevState.notes.filter( note => note.id !== action.payload)
+      }
+      return newState
+    }
     default:
       return prevState
   }
@@ -36,10 +43,15 @@ function App() {
     setNoteInput("")
    }
 
+   const deleteNote = (id) => {
+    dispatch({type: "DELETE_NOTE", payload: id})
+   }
+
    const dropNote = (e) => { 
     e.target.style.left = `${e.pageX - 50}px`
     e.target.style.top = `${e.pageY - 50}px`
     }
+
 
   return (
     <div className="app">
@@ -62,7 +74,9 @@ function App() {
                 draggable="true"
                 onDragEnd={dropNote}
            >
-            <div className="close">
+            <div className="close"
+                 onClick={()=>deleteNote(note.id)}
+            >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
               </svg>
